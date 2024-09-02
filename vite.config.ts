60833a4c6b8faf1e56f54c3e5c1d67548fa1db94
unknown <fred.skp2@gmail.com>
2024-09-02 11:55:01 +0100

    Build changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Change this from 'dist' to 'build'
-  },
-  base: '/',
+    outDir: 'build', // Ensure the output directory is set to 'build'
+  }
 })
+
